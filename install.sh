e3e268a8ac281ff4ec5d001d1e6d9af2cbe6db85
mon <gamerzahatv@gmail.com>
2022-05-01 12:20:41 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 allpackages=(
-    #"apache2"
-    #"php"
+    "apache2"
+    "php"
     "nano"
 )
 function setup(){
-	try || catch 
+	trysetup || catchsetup
+}
+function aptupdate(){
+	read -p "Do you want apt update && sudo apt upgrade [Y/N]: " result
+	result=${result:-N}
+	if [[ $result =~ ^[Yy]$ ]]; then
+		apt update && sudo apt upgrade
+	else
+		setup
+	echo $result
+	fi
 }
 
-function try(){
-	read -p "Enter your name [Y]: " name
+# try catch setup
+function trysetup(){
+	echo "ALL PACKAGES YOU WILL INSTALL"
+	for display in "${allpackages[@]}";
+		do
+			echo $display
+		done
+	read -p "Do you want  insall package [Y/N]: " name
 	name=${name:-y}
 	if [[ $name =~ ^[Yy]$ ]]; then
 		for task in "${allpackages[@]}";
 			do
-				sudo apt-get install $task
+				apt-get install $task
 			done
+	else
+		exit 0
 	fi
 }
-function catch(){
+function catchsetup(){
 	for task in "${allpackages[@]}";
 			do
-				apt-get install $task
+				sudo apt-get install $task
 			done
 }
 #call run
-setup
+aptupdate
